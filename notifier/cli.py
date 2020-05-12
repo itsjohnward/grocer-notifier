a7c5775d712362b153de09d4507f931003ecd3a5
John Ward <jsward2@gmail.com>
2020-05-12 00:24:45 -0400

    fix: resolve argparse bugs

--- notifier/cli.py
+++ notifier/cli.py
@@ -1,5 +1,9 @@
+import argparse
+import time
+
 from grocer import GrocerClient
-from twilio_utils import send
+
+from .twilio_utils import send
 
 
 def main():
@@ -7,16 +11,14 @@ def main():
 
     parser.add_argument("merchant", choices=["wegmans"])
     parser.add_argument(
-        "phone",
-        metavar="#",
-        type=int,
-        nargs=1,
-        help="the phone number to send alerts to",
+        "phone", metavar="#", type=int, help="the phone number to send alerts to",
     )
     parser.add_argument("-e", "--email", action="store", type=str)
     parser.add_argument("-p", "--password", action="store", type=str)
     args = parser.parse_args()
 
+    print(args.phone)
+    print(len(str(args.phone)))
     if len(str(args.phone)) != 11:
         raise ValueError(
             "Please enter a valid 11-digit phone number, including country code. i.e. 12223334444"
@@ -26,11 +28,19 @@ def main():
 
     previous_times = None
     while True:
-        times = grocer.get_delivery_times(merchant=args["merchant"])
+        times = client.get_delivery_times()
         # Send a message when availability changes:
         if len(times) > 0 and (previous_times is None or len(previous_times) == 0):
             send(
-                number=args["phone"],
-                message="\n".join([row for index, row in times.iterrows()]),
+                number=args.phone,
+                message="\n".join(
+                    [
+                        ",".join(
+                            [str(row["date"]), str(row["time"]), str(row["price"])]
+                        )
+                        for index, row in times.iterrows()
+                    ]
+                ),
             )
         previous_times = times
+        time.sleep(10)
