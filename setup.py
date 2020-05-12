c9862879d462c15d26a8efc13863e2d948e404cc
John Ward <jsward2@gmail.com>
2020-05-11 23:47:23 -0400

    feat: add setup.py to make tool installable

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import sys
+
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name="grocer-notifier",
+    description="Automatic alerts when your grocery delivery service has delivery times available",
+    long_description=long_description,
+    author="John Ward",
+    author_email="john@johnward.io",
+    url="https://github.com/itsjohnward/grocer-notifier",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "twilio",
+        "grocer @ git+https://git@github.com/itsjohnward/grocer@#egg=grocer",
+    ],
+    # root must be current directory
+    # otherwise, use_scm_version = {"root": path, "relative_to": __file__}
+    use_scm_version=True,
+    setup_requires=pytest_runner + ["setuptools_scm"],
+    test_suite="pytest",
+    tests_require=[],
+    entry_points={"console_scripts": ["grocerytime=notifier.__main__:main"]},
+)
